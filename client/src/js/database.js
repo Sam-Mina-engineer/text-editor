d627d4fd6ce6b2907db46da417529eba1bd5bfee
Sam Mina <samandmissy2010@gmail.com>
2024-11-29 15:07:21 -0500

    Implemented putDB and getDB methods in database js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,58 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+export const putDb = async (content) => {
+  console.log('PUT to db');
+
+  // Open db
+
+  const jateDb = await openDB('jate', 1);
+
+  // New transaction
+
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // Open object store
+
+  const store = tx.objectStore('jate');
+
+  // Update db content
+
+  const request = store.put({ id: 1, content });
+
+  // Confirmation
+
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Open db 
+
+  const jateDb = await openDB('jate', 1);
+
+  // New transaction
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Open object store
+
+  const store = tx.objectStore('jate');
+
+  // Get data
+
+  const request = store.get(1);
+
+  // Confirmation
+  
+  const result = await request;
+  console.log('Data retrieved from the database', result);
+  return result?.content;
+};
 
 initdb();
